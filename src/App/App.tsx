2d60e612618172c3ec07733a6fba7a90874c0351
한병국 <bg-shorthand@hanbyeong-gug-ui-MacBookPro.local>
2021-07-02 10:26:54 +0900

    [추가] google login

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,20 @@
+import { Route, Switch } from 'react-router-dom';
 import Signin from 'Pages/Signin/Signin';
+import Main from 'Pages/Main/Main';
+import { handleClientLoad } from 'gcp/gcp';
+import { useEffect } from 'react';
 
 function App() {
-  return <Signin />;
+  useEffect(() => {
+    handleClientLoad();
+  });
+
+  return (
+    <Switch>
+      <Route path="/main" component={Main} />
+      <Route path="/" exact component={Signin} />
+    </Switch>
+  );
 }
 
 export default App;
--- src/Pages/Signin/Signin.tsx
+++ src/Pages/Signin/Signin.tsx
@@ -1,8 +1,30 @@
+import { handleAuthClick, handleClientLoad, handleSignoutClick, isSign } from 'gcp/gcp';
+import { useHistory } from 'react-router-dom';
+
 const Signin = () => {
+  const history = useHistory();
+
+  const signin = async () => {
+    if (isSign()) {
+      history.push('/main');
+    } else {
+      try {
+        await handleAuthClick();
+        if (isSign()) history.push('/main');
+      } catch {
+        alert('알서포트 계정으로 로그인해야 합니다.');
+      }
+    }
+  };
+  const signout = async () => {
+    if (isSign()) handleSignoutClick();
+  };
+
   return (
     <>
       <h1>RESERVE MEET</h1>
-      <button>Signin</button>
+      <button onClick={signin}>Signin</button>
+      <button onClick={signout}>Signout</button>
     </>
   );
 };
