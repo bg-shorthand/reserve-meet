facbf4c90d73f6694467b60a89a510e9413ce456
한병국 <hanhanhan333@gmail.com>
2021-07-13 10:03:48 +0900

    [수정] 첫 화면에서 이주의 일정 표시

--- src/Containers/Main/Main.tsx
+++ src/Containers/Main/Main.tsx
@@ -2,42 +2,50 @@ import { useState } from 'react';
 import { Events } from 'const/type';
 import { useEffect } from 'react';
 import { calendarApi } from 'api/calendarApi';
+import getDate from 'module/getDate';
+import { useRecoilValue } from 'recoil';
+import { userState } from 'state/state';
 
 const Main = () => {
-  const [todayEvents, setTodayEvents] = useState([] as Events);
+  const [events, setEvents] = useState<Events>([]);
+  const { email } = useRecoilValue(userState);
 
   useEffect(() => {
-    let timerId: NodeJS.Timeout;
+    let examTimerId: NodeJS.Timeout;
 
     const exam = async () => {
-      const res = await calendarApi.getEvents();
-
-      clearTimeout(timerId);
-
-      if (res && res.result.items) {
-        console.log(res.result.items);
-        const newEvents = res.result.items.map((v: any) => ({
-          summary: v.summary,
-          location: v.location,
-          time: v.start.dateTime,
-          date: v.start.dateTime,
-        }));
-        setTodayEvents(newEvents);
-      } else {
-        timerId = setTimeout(exam, 100);
+      console.log(email);
+      const [start, end] = getDate.thisWeek();
+      clearTimeout(examTimerId);
+      if (email) {
+        const res = await calendarApi.getEvents(email, start, end);
+        if (res && res.result.items) {
+          const newEvents = res.result.items.map((v: any) => ({
+            summary: v.summary,
+            location: v.location,
+            time: v.start.dateTime,
+            date: v.start.dateTime,
+            id: v.id,
+          }));
+          setEvents(newEvents);
+        } else {
+          examTimerId = setTimeout(exam, 100);
+        }
       }
     };
     exam();
-  }, []);
+  }, [email]);
 
   return (
     <section>
       <h1>이번주 일정</h1>
-      <ul>
-        {todayEvents.map(event => {
-          return <li>{`${event.summary}, ${event.location}, ${event.time}`}</li>;
-        })}
-      </ul>
+      {events.length ? (
+        <ul>
+          {events.map(event => {
+            return <li key={event.id}>{`${event.summary}, ${event.location}, ${event.time}`}</li>;
+          })}
+        </ul>
+      ) : null}
     </section>
   );
 };
--- src/Pages/Main/Index.tsx
+++ src/Pages/Main/Index.tsx
@@ -2,41 +2,15 @@ import { useEffect, useState } from 'react';
 import { calendarApi } from 'api/calendarApi';
 import Menu from 'Containers/Menu/Menu';
 import Main from 'Containers/Main/Main';
+import getDate from 'module/getDate';
+import { useRecoilValue } from 'recoil';
+import { userState } from 'state/state';
 
 const Index = () => {
-  const [events, setEvents] = useState([{ location: '', summary: '', time: '' }]);
-
-  useEffect(() => {
-    let examTimerId: NodeJS.Timeout;
-
-    const exam = async () => {
-      const res = await calendarApi.getEvents();
-      if (res && res.result.items) {
-        clearTimeout(examTimerId);
-        console.log(res.result.items);
-        const newEvents = res.result.items.map((v: any) => ({
-          summary: v.summary,
-          location: v.location,
-          time: v.start.dateTime,
-        }));
-        setEvents(newEvents);
-      } else {
-        clearTimeout(examTimerId);
-        examTimerId = setTimeout(exam, 100);
-      }
-    };
-    exam();
-  }, []);
-
   return (
     <>
       <Menu />
       <Main />
-      <ul>
-        {events.map((event, i) => (
-          <li key={i}>{`요약: ${event.summary}, 장소: ${event.location}, 시간: ${event.time}`}</li>
-        ))}
-      </ul>
     </>
   );
 };
