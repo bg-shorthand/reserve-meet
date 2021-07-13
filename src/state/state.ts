fc61ba02e193f390e925a7b7d3e11b7dd1fbef2a
한병국 <hanhanhan333@gmail.com>
2021-07-13 15:43:49 +0900

    [추가] 캘린더 상태 추가

--- src/state/state.ts
+++ src/state/state.ts
@@ -1,8 +1,14 @@
 import { atom } from 'recoil';
+import { Calendars } from 'const/type';
 
 const userState = atom({
   key: 'userState',
   default: { name: '', imageUrl: '', email: '' },
 });
 
-export { userState };
+const calendarListState = atom({
+  key: 'calendarList',
+  default: [] as Calendars,
+});
+
+export { userState, calendarListState };
