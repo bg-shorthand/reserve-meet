25447424129efb180ec72c7a76a8896a6114b286
한병국 <hanhanhan333@gmail.com>
2021-07-13 09:57:09 +0900

    [수정] 유저 스테이트에 이메일 주소(프라이머리 캘린더 아이디) 추가

--- src/state/state.ts
+++ src/state/state.ts
@@ -2,7 +2,7 @@ import { atom } from 'recoil';
 
 const userState = atom({
   key: 'userState',
-  default: { name: '', imageUrl: '' },
+  default: { name: '', imageUrl: '', email: '' },
 });
 
 export { userState };
