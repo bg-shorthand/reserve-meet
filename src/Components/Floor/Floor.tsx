5854a0d6f5abee820a466edb8edbda5aa725b7dc
한병국 <hanhanhan333@gmail.com>
2021-07-15 09:44:51 +0900

    [수정] 상태 이름 변경(floorState -> curFloorState)
    1. floorState -> curFloorState
    2. 상태값의 타입도 변경 'floor9' -> 9

--- src/Components/Floor/Floor.tsx
+++ src/Components/Floor/Floor.tsx
@@ -2,15 +2,15 @@ import { DefaultProps } from 'const/type';
 import { useEffect } from 'react';
 import { ButtonHTMLAttributes, MouseEventHandler } from 'react';
 import { useRecoilState } from 'recoil';
-import { floorState } from 'state/state';
+import { curFloorState } from 'state/state';
 
 const Floor = ({ className }: DefaultProps) => {
   const floors = [9, 10, 11, 12, 15];
 
-  const [floor, setFloor] = useRecoilState(floorState);
+  const [floor, setFloor] = useRecoilState(curFloorState);
 
   const changeFloorHandler: MouseEventHandler<Element> = e => {
-    setFloor(e.currentTarget.id);
+    setFloor(+e.currentTarget.id);
   };
 
   useEffect(() => {
@@ -20,7 +20,7 @@ const Floor = ({ className }: DefaultProps) => {
   return (
     <ul className={className}>
       {floors.map(floor => (
-        <li key={floor} id={'floor' + floor} onClick={changeFloorHandler}>
+        <li key={floor} id={floor + ''} onClick={changeFloorHandler}>
           {floor + '층'}
         </li>
       ))}
--- src/state/state.ts
+++ src/state/state.ts
@@ -11,29 +11,29 @@ const calendarListState = atom({
   default: [] as Calendars,
 });
 
-const floorState = atom({
-  key: 'floorState',
-  default: 'floor9',
+const curFloorState = atom({
+  key: 'curFloorState',
+  default: 9,
 });
 
 const roomsState = selector({
   key: 'roomsState',
   get: ({ get }) => {
-    const floor = get(floorState);
+    const curFloor = get(curFloorState);
 
-    switch (floor) {
-      case 'floor9':
+    switch (curFloor) {
+      case 9:
         return ['대', 'M2', 'M3'].map(str => str + '회의실');
-      case 'floor10':
+      case 10:
         return ['대', '중', '소'].map(str => str + '회의실');
-      case 'floor11':
+      case 11:
         return ['대', 'Studio'].map(str => str + '회의실');
-      case 'floor12':
+      case 12:
         return ['소1', '소2'].map(str => str + '회의실');
-      case 'floor15':
+      case 15:
         return ['대', '소3', '소4'].map(str => str + '회의실');
     }
   },
 });
 
-export { userState, calendarListState, floorState, roomsState };
+export { userState, calendarListState, curFloorState, roomsState };
