33d8fc2156539e441f112d87bdaffa29594adc2b
한병국 <hanhanhan333@gmail.com>
2021-07-15 13:48:25 +0900

    [수정] ReserveTable에서 층, 날짜로 필터링 된 이벤트만 렌더링

--- src/Containers/Main/Contents/ReserveTable/ReserveTable.tsx
+++ src/Containers/Main/Contents/ReserveTable/ReserveTable.tsx
@@ -1,21 +1,22 @@
-import { calendarApi } from 'api/calendarApi';
-import { DefaultProps, Events } from 'const/type';
-import getDate from 'module/getDate';
 import { useEffect } from 'react';
-import { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { calendarApi } from 'api/calendarApi';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { curDateState, eventsState, renderEventsState, roomsState } from 'state/state';
+import { DefaultProps } from 'const/type';
+import getDate from 'module/getDate';
 import StyledTable from 'Components/Table/Table.style';
 import StyledFloor from 'Components/Floor/Floor.style';
 import DatePicker from 'Components/DatePicker/DatePicker';
-import { useRecoilValue } from 'recoil';
-import { roomsState } from 'state/state';
 
 type params = {
   calId: string;
 };
 
 const ReserveTable = ({ className }: DefaultProps) => {
-  const [events, setEvents] = useState<Events>([]);
+  const [events, setEvents] = useRecoilState(eventsState);
+  const renderEvents = useRecoilValue(renderEventsState);
+  const curDate = useRecoilValue(curDateState);
   const rooms = useRecoilValue(roomsState);
 
   const params: params = useParams();
@@ -27,25 +28,25 @@ const ReserveTable = ({ className }: DefaultProps) => {
     const getEvents = async () => {
       clearTimeout(timerId);
 
-      const [start, end] = getDate.thisMonth();
+      const [start, end] = getDate.today(curDate);
       const res = await calendarApi.getEvents(calId, start, end);
 
       if (res && res.result.items) {
-        console.log('events', res);
         const newEvents = res.result.items.map(({ id, summary, location, start }) => ({
           id: id ? id : '',
           summary: summary ? summary : '',
           location: location ? location : '',
-          date: start ? (start.dateTime ? start.dateTime : '') : '',
-          time: start ? (start.dateTime ? start.dateTime : '') : '',
+          date: start ? (start.dateTime ? start.dateTime.slice(0, 10) : '') : '',
+          time: start ? (start.dateTime ? start.dateTime.slice(11, 17) : '') : '',
         }));
+        console.log('newEvents', newEvents);
         setEvents([...newEvents]);
       } else {
         timerId = setTimeout(getEvents, 100);
       }
     };
     getEvents();
-  }, [params]);
+  }, [params, curDate]);
 
   return (
     <section className={className}>
@@ -53,9 +54,11 @@ const ReserveTable = ({ className }: DefaultProps) => {
       <StyledFloor />
       <h1>예약 현황</h1>
       <ul>
-        {events.length
+        {renderEvents.length
           ? events.map(event => (
-              <li key={event.id}>{`${event.summary}, ${event.location}, ${event.date}`}</li>
+              <li
+                key={event.id}
+              >{`${event.summary}, ${event.location}, ${event.date}, ${event.time}`}</li>
             ))
           : null}
       </ul>
