f6ac12e03d4412b3b403b6417a3d9d048caa1b57
bg-shorthand <hanhanhan333@gmail.com>
2021-07-16 10:04:58 +0900

    [추가] 테이블 셀 클릭시 newEvent 상태 업데이트

--- src/Components/Table/Table.tsx
+++ src/Components/Table/Table.tsx
@@ -1,11 +1,21 @@
 import { END_TIME, START_TIME } from 'const/const';
 import { DefaultProps } from 'const/type';
+import { MouseEventHandler } from 'react';
+import { useParams } from 'react-router-dom';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { curDateState, curFloorState, newEventState } from 'state/state';
 
 interface props extends DefaultProps {
   rooms: string[];
 }
+type params = { calId: '' };
 
 const Table = ({ className, rooms }: props) => {
+  const params = useParams<params>();
+  const curFloor = useRecoilValue(curFloorState);
+  const curDate = useRecoilValue(curDateState);
+  const setNewEvent = useSetRecoilState(newEventState);
+
   const timeTable = (() => {
     const temp: string[] = [];
     for (let i = START_TIME; i <= END_TIME; i++) {
@@ -13,6 +23,22 @@ const Table = ({ className, rooms }: props) => {
     }
     return temp;
   })();
+
+  const setNewEventHandler: MouseEventHandler<Element> = e => {
+    const calendarId = params.calId;
+    const date = curDate;
+    const floor = curFloor;
+    const [startTime, room] = e.currentTarget.id.split('-');
+    const endTime = startTime + 1;
+
+    setNewEvent(pre => ({
+      ...pre,
+      calendarId: calendarId.slice(1),
+      start: date + 'T' + startTime + ':00Z',
+      end: date + 'T' + endTime + ':00Z',
+      location: floor + '층 ' + room,
+    }));
+  };
 
   return (
     <table className={className}>
@@ -30,7 +56,7 @@ const Table = ({ className, rooms }: props) => {
             <tr key={i}>
               <th>{time}</th>
               {rooms.map((room, index) => (
-                <td key={index} id={time + '-' + room}></td>
+                <td key={index} id={time + '-' + room} onClick={setNewEventHandler}></td>
               ))}
             </tr>
           );
