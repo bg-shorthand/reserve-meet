7aa9e9ff0b61e0c0c51dd56d44b4357c281046f0
bg-shorthand <hanhanhan333@gmail.com>
2021-07-18 13:55:46 +0900

    [추가] 테이블에 일정 표시

--- src/Components/Table/Table.tsx
+++ src/Components/Table/Table.tsx
@@ -1,9 +1,16 @@
 import { MouseEventHandler } from 'react';
 import { useParams } from 'react-router-dom';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
-import { curDateState, curFloorState, isOpenState, newEventState } from 'state/state';
+import {
+  curDateState,
+  curFloorState,
+  isOpenState,
+  newEventState,
+  renderEventsState,
+} from 'state/state';
 import { DefaultProps } from 'const/type';
 import { END_TIME, START_TIME } from 'const/const';
+import { useEffect } from 'react';
 
 interface props extends DefaultProps {
   rooms: string[];
@@ -16,6 +23,7 @@ const Table = ({ className, rooms }: props) => {
   const curDate = useRecoilValue(curDateState);
   const setNewEvent = useSetRecoilState(newEventState);
   const setIsOpen = useSetRecoilState(isOpenState);
+  const renderEvents = useRecoilValue(renderEventsState);
 
   const timeTable = (() => {
     const temp: string[] = [];
@@ -49,6 +57,30 @@ const Table = ({ className, rooms }: props) => {
     }));
     setIsOpen(pre => ({ ...pre, addEvent: true }));
   };
+
+  useEffect(() => {
+    renderEvents.forEach(event => {
+      const $td = document.getElementById(`${event.startTime}-${event.location.split(' ')[1]}`);
+      if ($td) {
+        const tdStartHour = +$td.id.slice(0, 2) * 60;
+        const tdStartMin = +$td.id.slice(3, 5);
+        const endTimeHour = +event.endTime.slice(0, 2) * 60;
+        const endTimeMin = +event.endTime.slice(3, 5);
+        const rowspanCount = (endTimeHour + endTimeMin - tdStartHour - tdStartMin) / 30;
+
+        $td.setAttribute('rowspan', rowspanCount + '');
+        $td.textContent = event.summary;
+      }
+    });
+
+    return () => {
+      const $dts = document.querySelectorAll('td');
+      $dts.forEach(td => {
+        td.textContent = '';
+        td.setAttribute('rowspan', '');
+      });
+    };
+  }, [renderEvents]);
 
   return (
     <table className={className}>
