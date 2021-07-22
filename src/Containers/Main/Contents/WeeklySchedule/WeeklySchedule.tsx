510ef07ecb22b87ac7bbcc5ade42a994e6f96460
한병국 <hanhanhan333@gmail.com>
2021-07-22 13:23:33 +0900

    [추가]  회의실 없는 위클리 스케줄러

--- src/Containers/Main/Contents/WeeklySchedule/WeeklySchedule.tsx
+++ src/Containers/Main/Contents/WeeklySchedule/WeeklySchedule.tsx
@@ -1,14 +1,21 @@
 import { useState } from 'react';
-import { Events } from 'const/type';
+import { DefaultProps, Events } from 'const/type';
 import { useEffect } from 'react';
 import { calendarApi } from 'api/calendarApi';
 import getDate from 'module/getDate';
 import { useRecoilValue } from 'recoil';
 import { userState } from 'state/state';
 import { useParams } from 'react-router-dom';
+import { TIME_TABLE } from 'const/const';
 
-const WeeklySchedule = () => {
+const WeeklySchedule = ({ className }: DefaultProps) => {
   const [events, setEvents] = useState<Events>([]);
+  const [curWeek, setCurWeek] = useState(getDate.sundayToSaturday());
+
+  console.log(curWeek);
+
+  const [startAndEnd, setStartAndEnd] = useState(getDate.thisWeek());
+  const [start, end] = startAndEnd;
   const curUser = useRecoilValue(userState);
 
   const params = useParams<{ calId: string }>();
@@ -18,7 +25,6 @@ const WeeklySchedule = () => {
     let timerId: NodeJS.Timeout;
 
     const getEventsThisWeek = async () => {
-      const [start, end] = getDate.thisWeek();
       clearTimeout(timerId);
       if (email) {
         const res = await calendarApi.getEvents(email, start, end);
@@ -42,7 +48,7 @@ const WeeklySchedule = () => {
   }, [email]);
 
   return (
-    <section>
+    <section className={className}>
       <h1>이번주 일정</h1>
       {events.length ? (
         <ul>
@@ -55,6 +61,30 @@ const WeeklySchedule = () => {
           })}
         </ul>
       ) : null}
+      <table>
+        <thead>
+          <tr>
+            <th>시간</th>
+            {curWeek.map(date => (
+              <th id={date} key={date}>
+                {getDate.changeDateToDay(new Date(date))}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {TIME_TABLE.map((time, i) => {
+            return (
+              <tr key={i}>
+                <th>{time}</th>
+                {curWeek.map(date => (
+                  <td id={date.slice(0, 10) + 'T' + time} key={date}></td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </section>
   );
 };
--- src/Pages/Main/Index.tsx
+++ src/Pages/Main/Index.tsx
@@ -2,7 +2,7 @@ import { Route } from 'react-router';
 import { Switch, Redirect } from 'react-router-dom';
 import StyledReserveTable from 'Containers/Main/Contents/ReserveTable/ReserveTable.style';
 import StyledMenu from 'Containers/Main/Menu/Menu.style';
-import WeeklySchedule from 'Containers/Main/Contents/WeeklySchedule/WeeklySchedule';
+import StyledWeeklySchedule from 'Containers/Main/Contents/WeeklySchedule/WeeklySchedule.style';
 import StyledHeader from 'Containers/Header/Header.style';
 import StyledFooter from 'Containers/Footer/Footer.style';
 import StyledMain from 'Containers/Main/Main.style';
@@ -14,7 +14,7 @@ const Index = () => {
       <StyledMain>
         <StyledMenu />
         <Switch>
-          <Route exact path={'/'} component={WeeklySchedule} />
+          <Route exact path={'/'} component={StyledWeeklySchedule} />
           <Route path={'/reserve-state/:calId'} component={StyledReserveTable} />
           <Redirect to={'/'} />
         </Switch>
