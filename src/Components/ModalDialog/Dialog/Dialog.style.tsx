d282db9c4d4aaa250f44f37f2a01ee694b7f9996
bg-shorthand <hanhanhan333@gmail.com>
2021-07-27 20:40:52 +0900

    [수정] 다이얼로그 넓게

--- src/Components/ModalDialog/Dialog/Dialog.style.tsx
+++ src/Components/ModalDialog/Dialog/Dialog.style.tsx
@@ -15,7 +15,7 @@ const Dialog = ({ className, children }: DefaultProps) => {
 };
 
 const StyledDialog = styled(Dialog)`
-  width: 300px;
+  width: 500px;
   background-color: ${COLORS.GRAY_LEVEL_1};
   padding: 50px;
   border-radius: 10px;
