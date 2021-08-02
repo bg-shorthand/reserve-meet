0c70d5c775256dd956dea3b11c4602e8bc0aba0c
한병국 <hanhanhan333@gmail.com>
2021-08-02 15:58:58 +0900

    [추가] reserveTable 컴포넌트

---
+++ src/Components/RoomsTable/RoomsTable.tsx
@@ -0,0 +1,48 @@
+import { ReactComponent as CloseIcon } from 'asset/svg/close.svg';
+import { useRecoilState } from 'recoil';
+import { roomsState } from 'state/state';
+
+const RoomsTable = () => {
+  const [rooms, setRooms] = useRecoilState(roomsState);
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>층</th>
+            <th>회의실</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rooms.length
+            ? rooms.map(room => (
+                <tr key={room.id}>
+                  <th>{room.floor}</th>
+                  <td>
+                    <ul>
+                      {room.rooms.length
+                        ? room.rooms.map((room, index) => (
+                            <li key={room + index}>
+                              {room}
+                              <button>
+                                <CloseIcon />
+                              </button>
+                            </li>
+                          ))
+                        : null}
+                      <li>회의실 추가</li>
+                    </ul>
+                  </td>
+                </tr>
+              ))
+            : null}
+        </tbody>
+      </table>
+      <button>회의실 추가</button>
+      <button>수정</button>
+    </>
+  );
+};
+
+export default RoomsTable;
--- src/Containers/Main/Contents/Admin/Admin.tsx
+++ src/Containers/Main/Contents/Admin/Admin.tsx
@@ -1,50 +1,15 @@
-import { useRecoilState } from 'recoil';
-import { roomsState } from 'state/state';
 import { DefaultProps } from 'const/type';
-import { ReactComponent as CloseIcon } from 'asset/svg/close.svg';
 import AdminList from 'Components/AdminList/AdminList';
+import RoomsTable from 'Components/RoomsTable/RoomsTable';
 
 const Admin = ({ className }: DefaultProps) => {
-  const [rooms, setRooms] = useRecoilState(roomsState);
-
   return (
     <section className={className}>
       <h1>Admin</h1>
       <h2>관리자</h2>
       <AdminList />
       <h2>회의실</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>층</th>
-            <th>회의실</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rooms.map(room => (
-            <tr>
-              <th>{room.floor}</th>
-              <td>
-                <ul>
-                  {room.rooms.map(room => (
-                    <li>
-                      {room}
-                      <button>
-                        <CloseIcon />
-                      </button>
-                    </li>
-                  ))}
-                  <li>회의실 추가</li>
-                </ul>
-              </td>
-            </tr>
-          ))}
-          <tr>
-            <button>회의실 추가</button>
-          </tr>
-        </tbody>
-      </table>
-      <button>수정</button>
+      <RoomsTable />
     </section>
   );
 };
