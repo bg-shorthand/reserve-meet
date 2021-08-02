eda0f9056ff6528c3611f644d51821fc01242bc8
한병국 <hanhanhan333@gmail.com>
2021-08-02 16:15:58 +0900

    [추가] adminApi.delete, adminApi.post

--- src/api/db/adminApi.ts
+++ src/api/db/adminApi.ts
@@ -6,6 +6,12 @@ const adminApi = {
   async get() {
     return await axios.get(url);
   },
+  async delete(email: string) {
+    return await axios.delete(url + '/' + email);
+  },
+  async post(admins: { email: string }[]) {
+    return await axios.post(url, admins);
+  },
 };
 
 export default adminApi;
