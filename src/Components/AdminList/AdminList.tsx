4079ac34a666f78ea9cd6e990e6df20be499f94e
한병국 <hanhanhan333@gmail.com>
2021-08-02 16:16:15 +0900

    [추가] 어드민 삭제

--- src/Components/AdminList/AdminList.tsx
+++ src/Components/AdminList/AdminList.tsx
@@ -4,28 +4,40 @@ import { useRecoilState } from 'recoil';
 import { adminsState } from 'state/state';
 import { ReactComponent as CloseIcon } from 'asset/svg/close.svg';
 import SearchUser from 'Components/SearchUser/SearchUser';
+import { MouseEventHandler } from 'react';
+import adminApi from 'api/db/adminApi';
 
 const AdminList = () => {
   const [admins, setAdmins] = useRecoilState(adminsState);
   const [attendants, setAttendants] = useState<{ name: string; events: Events }[]>([]);
 
+  const deleteAdminHandler: MouseEventHandler<Element> = async e => {
+    const target = e.target as Element;
+    const email = target.closest('li')?.textContent;
+    if (email) {
+      const res = await adminApi.delete(email);
+      const newAdmins = await res.data;
+      setAdmins([...newAdmins]);
+    }
+  };
+
   return (
     <>
       <ul>
-        {admins.map(admin => (
-          <li key={admin.id}>
-            {admin.email}
-            <button>
-              <CloseIcon />
-            </button>
-          </li>
-        ))}
+        {admins.length
+          ? admins.map(admin => (
+              <li key={admin.id}>
+                {admin.email}
+                <button onClick={deleteAdminHandler}>
+                  <CloseIcon />
+                </button>
+              </li>
+            ))
+          : null}
       </ul>
       <SearchUser attendants={attendants} setAttendants={setAttendants} />
       <ul>
-        {attendants.map(user => (
-          <li>{user.name}</li>
-        ))}
+        {attendants.length ? attendants.map(user => <li key={user.name}>{user.name}</li>) : null}
       </ul>
       <button>관리자 추가</button>
     </>
