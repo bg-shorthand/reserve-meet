1d47eadb1f60410248a27969a3ea794e988dda9e
한병국 <hanhanhan333@gmail.com>
2021-08-04 13:11:58 +0900

    [추가] updateRoomsPerFloor

--- src/api/db/roomApi.ts
+++ src/api/db/roomApi.ts
@@ -6,6 +6,9 @@ const roomApi = {
   async get() {
     return await axios.get(url);
   },
+  async updateRoomsPerFloor(floor: number, roomsPerFloor: string[]) {
+    return await axios.put(url + '/' + floor, roomsPerFloor);
+  },
 };
 
 export default roomApi;
