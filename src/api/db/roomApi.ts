1aca5f5e2b95208ff3eb7169269c5b22ec4fab5a
bg-shorthand <hanhanhan333@gmail.com>
2021-08-05 13:14:40 +0900

    [추가] addFloor, deleteFloor

--- src/api/db/roomApi.ts
+++ src/api/db/roomApi.ts
@@ -6,6 +6,12 @@ const roomApi = {
   async get() {
     return await axios.get(url);
   },
+  async deleteFloor(floor: number) {
+    return await axios.delete(url + '/' + floor);
+  },
+  async addFloor(floor: number) {
+    return await axios.post(url, { floor });
+  },
   async updateRoomsPerFloor(floor: number, roomsPerFloor: string[]) {
     return await axios.put(url + '/' + floor, roomsPerFloor);
   },
