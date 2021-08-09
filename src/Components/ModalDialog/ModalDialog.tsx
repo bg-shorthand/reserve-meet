edcc0b0d9dc62d7def23de080c2c11466f880f19
한병국 <hanhanhan333@gmail.com>
2021-08-09 10:00:46 +0900

    [수정] 스피너의 isOpen 초기값이 true라 초기화시 스피너가 보이던 문제

--- src/Components/ModalDialog/ModalDialog.tsx
+++ src/Components/ModalDialog/ModalDialog.tsx
@@ -1,6 +1,6 @@
 import Portal from 'Portal';
 import { MouseEventHandler } from 'react';
-import { useResetRecoilState } from 'recoil';
+import { useResetRecoilState, useSetRecoilState } from 'recoil';
 import { isOpenState } from 'state/state';
 import { DefaultProps } from 'const/type';
 import StyledDialog from './Dialog/Dialog.style';
@@ -8,10 +8,12 @@ import StyledModal from './Modal/Modal.style';
 
 const ModalDialog = ({ className, children }: DefaultProps) => {
   const resetIsOpen = useResetRecoilState(isOpenState);
+  const setIsOpen = useSetRecoilState(isOpenState);
 
   const closeModal: MouseEventHandler<Element> = e => {
     if (e.currentTarget !== e.target) return;
     resetIsOpen();
+    setIsOpen(pre => ({ ...pre, spinner: false }));
   };
 
   return (
