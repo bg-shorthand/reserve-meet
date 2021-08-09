9d827293759a88cf445576eacff7c924b3f6fd5e
한병국 <hanhanhan333@gmail.com>
2021-08-09 14:10:45 +0900

    [추가] 키보드 ui(tab키로 이동, esc키로 닫기

--- src/Components/ModalDialog/Dialog/Dialog.style.tsx
+++ src/Components/ModalDialog/Dialog/Dialog.style.tsx
@@ -2,14 +2,46 @@ import styled from 'styled-components';
 import { ReactComponent as CloseIcon } from 'asset/svg/close.svg';
 import { DefaultProps } from 'const/type';
 import { COLORS } from 'const/const';
+import { KeyboardEventHandler } from 'react';
+import { useRef } from 'react';
+import { useEffect } from 'react';
+import { useResetRecoilState, useSetRecoilState } from 'recoil';
+import { isOpenState } from 'state/state';
 
 const Dialog = ({ className, children }: DefaultProps) => {
+  const ref = useRef(null);
+  const resetIsOpen = useResetRecoilState(isOpenState);
+  const setIsOpen = useSetRecoilState(isOpenState);
+
+  const focusFirstHandler: KeyboardEventHandler = e => {
+    if (e.key !== 'Tab') return;
+    e.preventDefault();
+    const $modal = ref.current as unknown as HTMLElement;
+    const $firstFocusableElement = $modal.querySelectorAll(
+      'button:not(disabled), input, textarea, select',
+    )[0] as HTMLElement;
+    $firstFocusableElement.focus();
+  };
+  const closeByEscHandler: KeyboardEventHandler = e => {
+    if (e.key !== 'Escape') return;
+    resetIsOpen();
+    setIsOpen(pre => ({ ...pre, spinner: false }));
+  };
+
+  useEffect(() => {
+    const $modal = ref.current as unknown as HTMLElement;
+    const $firstFocusableElement = $modal.querySelectorAll(
+      'button:not(disabled), input, textarea, select',
+    )[0] as HTMLElement;
+    $firstFocusableElement.focus();
+  }, []);
+
   return (
-    <section className={className}>
-      <button>
+    <section className={className} ref={ref} onKeyDown={closeByEscHandler}>
+      {children}
+      <button onKeyDown={focusFirstHandler}>
         <CloseIcon />
       </button>
-      {children}
     </section>
   );
 };
@@ -21,7 +53,7 @@ const StyledDialog = styled(Dialog)`
   border-radius: 10px;
   position: relative;
 
-  & > button:first-child {
+  & > button:last-child {
     position: absolute;
     top: 20px;
     right: 20px;
