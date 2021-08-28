5976e74e804daede3d247a9872f480ac4b5c8082
한병국 <hanhanhan333@gmail.com>
2021-08-28 12:44:46 +0900

    [수정] 콘솔 로그 삭제

--- src/api/db/roomApi.ts
+++ src/api/db/roomApi.ts
@@ -5,7 +5,6 @@ const url = 'http://localhost:4001/room';
 const roomApi = {
   async get() {
     const res = await axios.get(url);
-    console.log(res);
     return res;
   },
   async deleteFloor(floor: number) {
