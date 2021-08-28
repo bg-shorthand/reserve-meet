312264f876feb899fe41f4360673e84690cc5c5a
한병국 <hanhanhan333@gmail.com>
2021-08-28 15:36:10 +0900

    [수정] 회의db를 날짜별로 구분해 관리

--- server/meeting/model.js
+++ server/meeting/model.js
@@ -2,22 +2,44 @@ const mongooseMeeting = require('mongoose');
 
 const meetingSchema = new mongooseMeeting.Schema(
   {
-    summary: { type: String, required: true },
-    description: { type: String, required: true },
-    location: { type: String, required: true },
-    creator: { type: Object, required: true },
-    start: { type: Object, required: true },
-    end: { type: Object, required: true },
-    attendees: { type: Array, required: true },
+    date: { type: String, required: true },
+    meetings: {
+      type: [
+        {
+          summary: { type: String, required: true },
+          description: { type: String, required: true },
+          location: { type: String, required: true },
+          creator: { type: Object, required: true },
+          start: { type: Object, required: true },
+          end: { type: Object, required: true },
+          attendees: { type: Array, required: true },
+        },
+      ],
+      required: true,
+    },
   },
   {
     versionKey: false,
   },
 );
 
-meetingSchema.statics.create = function (payload) {
-  const meeting = new this(payload);
-  return meeting.save();
+meetingSchema.statics.create = async function (payload) {
+  const date = payload.start.dateTime.slice(0, 10);
+  const meeting = await this.findOne({ date });
+  if (meeting) {
+    await this.updateOne(
+      { date },
+      {
+        $set: {
+          date,
+          meetings: [...meeting.meetings, payload],
+        },
+      },
+    );
+  } else {
+    const meeting = new this({ date: payload.start.dateTime.slice(0, 10), meetings: [payload] });
+    return meeting.save();
+  }
 };
 
 meetingSchema.statics.findAll = function () {
