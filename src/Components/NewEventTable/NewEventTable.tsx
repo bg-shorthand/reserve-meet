fb5420b6a0eb26badd2fc8e85bd1a1a4bb23436a
한병국 <hanhanhan333@gmail.com>
2021-09-06 16:33:18 +0900

    [수정] 회의 시작, 종료 시간 컨트롤

--- src/Components/NewEventTable/NewEventTable.tsx
+++ src/Components/NewEventTable/NewEventTable.tsx
@@ -1,5 +1,8 @@
 import { TIME_TABLE } from 'const/const';
+import addPrefix0 from 'module/addPrefix0';
 import createEndTimes from 'module/createEndTimes';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { ChangeEventHandler } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { newEventState, renderEventsState, roomsState } from 'state/state';
@@ -9,6 +12,9 @@ const NewEventTable = () => {
   const rooms = useRecoilValue(roomsState);
   const [newEvent, setNewEvent] = useRecoilState(newEventState);
   const { floor, room, startDate, startTime, endTime } = newEvent;
+
+  const [startTimes, setStartTimes] = useState<string[]>([]);
+  const [endTimes, setEndTimes] = useState<string[]>([]);
 
   const changeFloorHandler: ChangeEventHandler<HTMLSelectElement> = e => {
     const firstRoomPerFloor = rooms.find(room => room.floor === +e.currentTarget.value)
@@ -26,11 +32,43 @@ const NewEventTable = () => {
     }));
   };
   const changeStartTimeHandler: ChangeEventHandler<HTMLSelectElement> = e => {
-    setNewEvent(pre => ({ ...pre, startTime: e.currentTarget.value }));
+    setNewEvent(pre => ({
+      ...pre,
+      startTime: e.currentTarget.value,
+      endTime:
+        e.currentTarget.value >= endTime
+          ? e.currentTarget.value.slice(3, 5) === '00'
+            ? +e.currentTarget.value.slice(0, 2) + ':30'
+            : +e.currentTarget.value.slice(0, 2) + 1 + ':00'
+          : pre.endTime,
+    }));
   };
   const changeEndTimeHandler: ChangeEventHandler<HTMLSelectElement> = e => {
     setNewEvent(pre => ({ ...pre, endTime: e.currentTarget.value }));
   };
+
+  useEffect(() => {
+    console.log(newEvent);
+  }, [newEvent]);
+
+  useEffect(() => {
+    const date = new Date();
+    const curYear = date.getFullYear();
+    const curMonth = addPrefix0(date.getMonth() + 1);
+    const curDate = addPrefix0(date.getDate());
+    const curHour = addPrefix0(date.getHours());
+    const curMinute = addPrefix0(date.getMinutes());
+
+    if (startDate === curYear + '-' + curMonth + '-' + curDate) {
+      setStartTimes(TIME_TABLE.filter(time => time > curHour + ':' + curMinute));
+    } else {
+      setStartTimes([...TIME_TABLE]);
+    }
+  }, []);
+
+  useEffect(() => {
+    setEndTimes(createEndTimes(startTime, renderEvents, floor, room));
+  }, [startTime]);
 
   return (
     <table>
@@ -92,7 +130,7 @@ const NewEventTable = () => {
               value={startTime}
               onChange={changeStartTimeHandler}
             >
-              {TIME_TABLE.map(time => {
+              {startTimes.map(time => {
                 return (
                   <option value={time} key={time}>
                     {time}
@@ -112,8 +150,10 @@ const NewEventTable = () => {
                 value={endTime}
                 onChange={changeEndTimeHandler}
               >
-                {createEndTimes(startTime, renderEvents, floor, room).map(time => (
-                  <option key={time}>{time}</option>
+                {endTimes.map(time => (
+                  <option value={time} key={time}>
+                    {time}
+                  </option>
                 ))}
               </select>
             }
